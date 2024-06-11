401b06abcddd70de3912f14fb849590cbf943b2f
Lee Katz - Aspen <gzu2@cdc.gov>
2024-06-11 12:44:11 -0400

    misc

--- scripts/cecaelia.pl
+++ scripts/cecaelia.pl
@@ -31,103 +31,124 @@ sub main{
 
 sub testForUnevenness{
   my($infile, $settings) = @_;
-    # kraken.out now has a file with, e.g., 
-    #    C	chimera_of_K12_and_MN908947	562	4671555	562:14 0:35 562:121 0:63 562:34 0:40
-    # And now we need to translate all of that to the species or genus level,
-    # using taxonkit.
-    my $targetRank = 'species';
-    # Cache taxonkit results
-    my %taxonkitLineageCache;
-    open(my $krakenFh, '<', $infile) or die "Could not read '$infile' $!";
-    while(my $line = <$krakenFh>){
-        chomp $line;
-        my($classified, $seqname, $consensusTaxid, $seqlength, $kraken_line) = split(/\t/, $line);
-        # Make an array of taxids at the target rank 
-        my @taxidsAlongSeq;
-        for my $lcaMapping(split(/ +/, $kraken_line)){
-            my($taxid, $count) = split(/:/, $lcaMapping);
-
-            # Take care of the special case where Kraken didn't classify, 
-            # which is indicated with 0
-            if($taxid == 0){
-                push(@taxidsAlongSeq, $taxid);
-                next;
-            }
-
-            # Make a hash of $rank => $taxid
-            my %rank;
-            my $lineage = $taxonkitLineageCache{$taxid};
-            if(!defined($lineage)){
-                logmsg "Taxonkit lineage -R with $taxid";
-                $lineage = `echo $taxid | taxonkit lineage -R -t`;
-                chomp($lineage);
-                $taxonkitLineageCache{$taxid} = $lineage;
-            } 
-
-            # Parse the lineage. Might want to cache this part later if I want to eek out a little more speed.
-            my(undef, $scinames, $taxids, $ranks) = split(/\t/, $lineage);
-            my @sciname = split(/;/, $scinames);
-            my @rank = split(/;/, $ranks);
-            my @taxid = split(/;/, $taxids);
-            #if($taxid == 2697049){
-            #  die Dumper [$count, \@sciname, \@rank, \@taxid];
-            #}
-            for(my $i=0; $i<@rank; $i++){
-                $rank{$rank[$i]} = $taxid[$i];
-                $rank{$rank[$i]} //= 0; # set to 0 if not defined
-            }
-            for(1..$count){
-              push(@taxidsAlongSeq, $rank{$targetRank});
-            }
-        }
-
-        logmsg "We have ".scalar(@taxidsAlongSeq)." positions from kraken for seqname '$seqname'";
-        
-        # make the chi square table.
-        # Pick a position and then how many flanking positions to look at
-        my $position = 4550000;
-        my $flank = 14000;
-        my $start = $position - $flank;
-        my $stop  = $position + $flank;
-
-        # Get the "before" and "after"
-        my %countsBefore;
-        my %countsAfter;
-        for my $i($start..$position-1){
-            my $taxid = $taxidsAlongSeq[$i];
-            die "ERROR: taxid is not defined for position $i" if(!defined($taxid));
-            $countsBefore{$taxid}++;
-        }
-        for my $i($position+1..$stop){
-            my $taxid = $taxidsAlongSeq[$i];
-            die "ERROR: taxid is not defined for position $i" if(!defined($taxid));
-            $countsAfter{$taxid}++;
-        }
-
-        # how many are "before" in the major category?
-        my $majorCategory1 = 562;    # E. coli
-        my $majorCategory2 = 694009; # Severe acute respiratory syndrome-related coronavirus
-        my $missingCategory= 1;
-
-        # chi square table with pseudocounts
-        my @chiTable = (
-          [
-            $countsBefore{$majorCategory1} //  1,
-            $countsBefore{$majorCategory2} //  1,
-            $countsBefore{$missingCategory} // 1,
-          ],
-          [
-            $countsAfter{$majorCategory1} //   1,
-            $countsAfter{$majorCategory2} //   1,
-            $countsAfter{$missingCategory} //  1,
-          ]
-        );
-        my $chi = new Statistics::ChisqIndep;
-        $chi->load_data(\@chiTable);
-        $chi->print_summary();die;
-
-        print Dumper [$position,$flank, \%countsBefore, \%countsAfter, \@chiTable, [chisquare(@chiTable)]];
+  # kraken.out now has a file with, e.g., 
+  #    C	chimera_of_K12_and_MN908947	562	4671555	562:14 0:35 562:121 0:63 562:34 0:40
+  # And now we need to translate all of that to the species or genus level,
+  # using taxonkit.
+  my $targetRank = 'species';
+  # Cache taxonkit results: this will be modified by reference
+  my %taxonkitLineageCache;
+  open(my $krakenFh, '<', $infile) or die "Could not read '$infile' $!";
+  while(my $line = <$krakenFh>){
+    chomp $line;
+    my $taxidsAlongSeq = findTaxIds($line, $targetRank, \%taxonkitLineageCache, $settings);
+    # Find the majority taxid and then see if anything disagrees with it
+    my %taxidCount;
+    for(@$taxidsAlongSeq){
+      $taxidCount{$_}++;
     }
+    my @sortedTaxids = sort {$taxidCount{$b} <=> $taxidCount{$a}} keys(%taxidCount);
+    my $majorityTaxid = $sortedTaxids[0];
+    if($majorityTaxid == 0){
+      $majorityTaxid = $sortedTaxids[1];
+    }
+    print Dumper \%taxidCount, $majorityTaxid;
+    ...;
+
+    logmsg "We have ".scalar(@$taxidsAlongSeq)." positions from kraken";
+    
+    # make the chi square table.
+    # Pick a position and then how many flanking positions to look at
+    my $position = 4550000;
+    my $flank = 14000;
+    my $start = $position - $flank;
+    my $stop  = $position + $flank;
+
+    # Get the "before" and "after"
+    my %countsBefore;
+    my %countsAfter;
+    for my $i($start..$position-1){
+        my $taxid = $$taxidsAlongSeq[$i];
+        die "ERROR: taxid is not defined for position $i" if(!defined($taxid));
+        $countsBefore{$taxid}++;
+    }
+    for my $i($position+1..$stop){
+        my $taxid = $$taxidsAlongSeq[$i];
+        die "ERROR: taxid is not defined for position $i" if(!defined($taxid));
+        $countsAfter{$taxid}++;
+    }
+
+    # how many are "before" in the major category?
+    my $majorCategory1 = 562;    # E. coli
+    my $majorCategory2 = 694009; # Severe acute respiratory syndrome-related coronavirus
+    my $missingCategory= 1;
+
+    # chi square table with pseudocounts
+    my @chiTable = (
+      [
+        $countsBefore{$majorCategory1} //  1,
+        $countsBefore{$majorCategory2} //  1,
+        $countsBefore{$missingCategory} // 1,
+      ],
+      [
+        $countsAfter{$majorCategory1} //   1,
+        $countsAfter{$majorCategory2} //   1,
+        $countsAfter{$missingCategory} //  1,
+      ]
+    );
+    my $chi = new Statistics::ChisqIndep;
+    $chi->load_data(\@chiTable);
+    $chi->print_summary();die;
+
+    print Dumper [$position,$flank, \%countsBefore, \%countsAfter, \@chiTable, [chisquare(@chiTable)]];
+  }
+}
+
+# Gets taxids of the kraken line at the target rank
+sub findTaxIds{
+  my($line, $targetRank, $taxonkitLineageCache, $settings) = @_;
+  my($classified, $seqname, $consensusTaxid, $seqlength, $kraken_line) = split(/\t/, $line);
+
+  # Make an array of taxids at the target rank 
+  my @taxidsAlongSeq;
+  for my $lcaMapping(split(/ +/, $kraken_line)){
+    my($taxid, $count) = split(/:/, $lcaMapping);
+
+    # Take care of the special case where Kraken didn't classify, 
+    # which is indicated with 0
+    if($taxid == 0){
+        push(@taxidsAlongSeq, $taxid);
+        next;
+    }
+
+    # Make a hash of $rank => $taxid
+    my %rank;
+    my $lineage = $$taxonkitLineageCache{$taxid};
+    if(!defined($lineage)){
+        logmsg "Taxonkit lineage -R with $taxid";
+        $lineage = `echo $taxid | taxonkit lineage -R -t`;
+        chomp($lineage);
+        $$taxonkitLineageCache{$taxid} = $lineage;
+    } 
+
+    # Parse the lineage. Might want to cache this part later if I want to eek out a little more speed.
+    my(undef, $scinames, $taxids, $ranks) = split(/\t/, $lineage);
+    my @sciname = split(/;/, $scinames);
+    my @rank = split(/;/, $ranks);
+    my @taxid = split(/;/, $taxids);
+    #if($taxid == 2697049){
+    #  die Dumper [$count, \@sciname, \@rank, \@taxid];
+    #}
+    for(my $i=0; $i<@rank; $i++){
+        $rank{$rank[$i]} = $taxid[$i];
+        $rank{$rank[$i]} //= 0; # set to 0 if not defined
+    }
+    for(1..$count){
+      push(@taxidsAlongSeq, $rank{$targetRank});
+    }
+  }
+
+  return \@taxidsAlongSeq;
 }
 
 sub usage{
--- t/01_index.t
+++ t/01_index.t
@@ -11,7 +11,7 @@ use Test::More tests => 2;
 
 $ENV{PATH} = "$RealBin/../scripts:".$ENV{PATH};
 
-diag `template.pl -h`;
+diag `cecaelia.pl -h`;
 my $exit_code = $? << 8;
 is($exit_code, 0, "exit code");
 
@@ -21,17 +21,25 @@ subtest 'kraken2' => sub {
     # Make the chimera file
     my $chimera = "$RealBin/data/workdir/chimera.fasta";
     open(my $fh, '>', $chimera) or die "Could not write to '$chimera' $!";
-    print $fh ">chimera_of_K12_and_MN908947\n";
+    my $numBp = 1000; # number of bp to extract from each genome
+    print $fh ">chimera_of_K12_and_MN908947 $numBp nucleotides from each assembly\n";
     for my $asm("$RealBin/data/K12.fasta", "$RealBin/data/MN908947.fasta") {
+        my $fastaStr = "";
         open(my $fh2, '<', $asm) or die "Could not read '$asm' $!";
-        while(<$fh2>) {
-            next if(/^>/);
-            print $fh $_;
+        # read the fasta file into memory
+        my $seqname = <$fh2>;
+        my $sequence = <$fh2>;
+        while(length($sequence) < $numBp){
+            $sequence .= <$fh2>;
+            # TODO if needed: detect EOF for $fh2
         }
+        $sequence =~ s/\s+//g;
+        $sequence = substr($sequence, 0, $numBp);
+        print $fh $sequence ."\n";
         close $fh2;
     }
     close $fh;
-    isnt(-s $chimera, 0, "chimera file is empty");
+    isnt(-s $chimera, 0, "chimera file isn't empty");
 
     # Run kraken
     my $out = "$RealBin/data/workdir/kraken.out";
